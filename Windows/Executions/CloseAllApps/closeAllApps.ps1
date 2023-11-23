740eb29b93b3eb855e1f0be8e06ac97570c75b63
Isaac Sanz <isaacsanzgomez102125@gmail.com>
2023-11-23 22:12:30 +0100

    Update

--- Windows/Executions/CloseAllApps/closeAllApps.ps1
+++ Windows/Executions/CloseAllApps/closeAllApps.ps1
@@ -1,5 +1,5 @@
 # Download Python script
-$scriptUrl = "https://raw.githubusercontent.com/Zenin0/Glitter_Scripts/main/Windows/CloseAllApps/script.py?token=GHSAT0AAAAAACJCV62UA5IVM6QMUZ6KX3UWZJVSG2Q"
+$scriptUrl = "https://raw.githubusercontent.com/Zenin0/Glipper_Scripts/main/Windows/Executions/CloseAllApps/script.py"
 $savePath = "$env:temp\script.py"
 (New-Object System.Net.WebClient).DownloadFile($scriptUrl, $savePath)
 
